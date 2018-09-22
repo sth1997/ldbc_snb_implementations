31e208d82e2dcbb395a7e1a6cd697957d42a3988
Antal János Benjamin <antal.janos.benjamin@gmail.com>
2018-09-22 17:24:51 +0200

    Remove typo

--- tinkerpop/src/main/java/com/ldbc/impls/workloads/ldbc/snb/tinkerpop/TinkerPopLoader.java
+++ tinkerpop/src/main/java/com/ldbc/impls/workloads/ldbc/snb/tinkerpop/TinkerPopLoader.java
@@ -18,7 +18,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-j
+
 public class TinkerPopLoader {
 
     private static final Logger logger =
