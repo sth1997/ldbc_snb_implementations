86fa9cb8839db2a47ab0b89c8bef6491d2c3002d
Tianhui Shi <765215342@qq.com>
2019-08-18 13:31:15 +0800

    Update Query1 properties.

---
+++ tinkerpop3/experiment.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import subprocess
+import time
+
+def run_cmd(cmd):
+	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	while p.poll() is None:
+		line = p.stdout.readline()
+		line = line.strip()
+		if line:
+			print(line)
+
+
+def run(threads_count, operation_count):
+#	run_cmd("sudo -E ./load-scripts/delete-neo4j-database.sh")
+	print("Delete database done.")
+#	run_cmd("sudo -E ./load-scripts/import-to-neo4j.sh")
+	print("Import data done.")
+#	run_cmd("sudo -E ./load-scripts/restart-neo4j.sh")
+	print("Restart neo4j done.")
+#	time.sleep(20)  #wait for neo4j ready
+	cmd = "java -cp target/tinkerpop3-0.0.1-SNAPSHOT.jar com.ldbc.driver.Client -P interactive-benchmark.properties -rd result/result-t{}-oc{}-newconf -tc {} -oc {}".format(threads_count, operation_count, threads_count,operation_count)
+	run_cmd(cmd)
+
+#threads_count = [1, 2, 4, 8, 16]
+threads_count = [1]
+operation_count = 1
+for tc in threads_count:
+	run(tc, operation_count)
+
---
+++ tinkerpop3/src/main/java/com/ldbc/impls/workloads/ldbc/snb/tinkerpop3/interactive/Tinkerpop3InteractiveDb.java
@@ -0,0 +1,16 @@
+package com.ldbc.impls.workloads.ldbc.snb.tinkerpop3.interactive;
+
+import com.ldbc.driver.DbException;
+import com.ldbc.driver.control.LoggingService;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.*;
+import com.ldbc.impls.workloads.ldbc.snb.tinkerpop3.Tinkerpop3Db;
+
+import java.util.Map;
+
+public class Tinkerpop3InteractiveDb extends Tinkerpop3Db{
+    @Override
+    protected void onInit(Map<String, String> properties, LoggingService loggingService) throws DbException {
+        super.onInit(properties, loggingService);
+        registerOperationHandler(LdbcQuery1.class, InteractiveQuery1.class);
+    }
+}
